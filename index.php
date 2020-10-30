042c4865cebc0366fdc4aac37f1ccab5108204dc
Butsyamas63070098 <73689381+Butsyamas63070098@users.noreply.github.com>
2020-10-30 15:31:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "<h2>บุษยมาส อวะภาค ชื่อเเล่น :ข้าว </h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <img src ="cat.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+
+    </body>
+</html>
