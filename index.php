5f9d58e1edf722034abc03b7c84148045ab774c2
Butsyamas63070098 <73689381+Butsyamas63070098@users.noreply.github.com>
2020-10-30 15:37:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,17 @@
         
     </head>
     <body>
-        <h1>My First Heading</h1>
-        <p>My First Paragraph</p>
+        <h1>บุษยมาส อวะภาค ชื่อเล่น ข้าว</h1>
+        <p>Are เป็น<br>
+           You คุณ<br>
+           Are you one know สุขัง พ้าลัง<br>
+        </p>
+        
         <img src ="cat.jpg">
+        <img src ="cat2.jpg">
         <br>
-        <a href="http://www.it.kmitl.ac.th">This is a Link</a>
-
+        <a href="https://twitter.com/">This is a Link</a>
         <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
-
+        
     </body>
 </html>
